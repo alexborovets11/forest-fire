dc33b7b6f79d65d3b9f2ef4f3ed846bb1906c1b1
Kabul Totakhil <totakhilkamran7@gmail.com>
2025-06-05 11:10:15 -0700

    change the direction code to the correct form

--- src/Fire.java
+++ src/Fire.java
@@ -50,16 +50,16 @@ public class Fire {
 
 
          int[][] directions = {
-            { -1, 0 }, // up
-            {  1, 0 }, // down
-            {  0, -1}, // left
-            {  0, 1 }  // right
+             {0, 1}, 
+             {1, 0}, 
+             {0, -1},
+            {-1, 0} };
         };
 
 
         
         Queue<int[]> queue = new LinkedList<>();
-        
+
         boolean[][] visited = new boolean[rows][cols];
 
         
